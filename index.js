144ff7fae3a74f54531c94aa59f48a9340cf2728
loevel <davechendjou@gmail.com>
2024-02-03 03:42:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = 3000; // Le port sur lequel vous souhaitez lancer le serveur
+
+server.use(middlewares);
+server.use(router);
+
+/*
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});*/
